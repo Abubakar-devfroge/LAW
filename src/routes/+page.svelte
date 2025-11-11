0b7b622b963f9fc5b0ba280881aced0ce50d5e9e
Abubakar devfroge <info.devforge@gmail.com>
2025-11-11 21:14:06 +0300

    second commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Cta from "$lib/components/Cta.svelte";
 	import Feature from "$lib/components/Feature.svelte";
 	import Hero from "$lib/components/Hero.svelte";
 	import Report from "$lib/components/Report.svelte";
@@ -8,3 +9,4 @@
 <Hero />
 <Feature />
 <Report />
+<Cta />
